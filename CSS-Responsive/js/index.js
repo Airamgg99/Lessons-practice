70e5ccfabf1cd6f0a8d5b7a2e7847fddc9db1fe1
Airam <156690928+Airamgg99@users.noreply.github.com>
2024-05-19 23:23:59 +0100

    index.js

--- CSS-Responsive/js/index.js
+++ CSS-Responsive/js/index.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuNav = document.getElementById("menu-nav");
+  menuToggle.addEventListener("click", function() {
+    menuNav.classList.toggle("menu-toggle");
+  });
+});
